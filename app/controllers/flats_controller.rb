8af8974bbb51dcfedfe034c1876d8017bd65f0ae
gabriel pacheco <gabrielagustinpacheco@gmail.com>
2020-09-09 17:04:00 +0200

    seeded the DB and started to code the actions in controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,23 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:flat).permit(:name, :address, :)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying  my flats"
+Flat.destroy_all
+
+puts "Creating new ones"
+
+5.times do
+  new_flat = Flat.new(
+    name: Faker::Games::SonicTheHedgehog.character,
+    address: Faker::Address.full_address,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: rand(50..150),
+    number_of_guests: rand(1..10)
+  )
+  new_flat.save!
+end
+
+puts "I'm done"
